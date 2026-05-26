06ebcb736267cb61858fa8cac8d149980b3896bc
Marco Dominguez <mardom640@gmail.com>
2026-05-26 17:51:39 -0600

    Initial Commit

---
+++ PROYECTO_FINAL/PROYECTO_FINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROYECTO_FINALApp.swift
+//  PROYECTO_FINAL
+//
+//  Created by Marco Dominguez on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PROYECTO_FINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
